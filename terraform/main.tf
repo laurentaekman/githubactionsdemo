997edf569ad2e97d2fd3bac1b440ca8030f17199
Lauren Taekman <lauren.taekman@willowtreeapps.com>
2022-09-12 16:40:54 -0400

    introducing terraform and attempting to run it

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+    features {}
+}
+
+variable "rg_prefix" {
+    default = "rg"
+}
+
+variable "location" {
+    default = "eastus"
+}
+
+resource "random_pet" "gh-actions-resource" {
+    prefix = var.rg_prefix
+}
+
+resource "azurerm_resource_group" "gh-actions-resource-group" {
+    location = var.location
+    name = random_pet.gh-actions-resource.id
+}
+
+output "resource_group_location" {
+    value = azurerm_resource_group.gh-actions-resource-group.name
+}
